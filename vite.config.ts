0add71f233c29a3ef5470b10934c60750b11e2a7
BHABANISHANKAR-01 <your-email@example.com>
2026-01-08 10:26:41 +0530

    Update repository configuration for new GitHub account

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/blockchainvisualiser/',
+  base: '/BlockchainVisualiser/',
   plugins: [react()],
   optimizeDeps: {
     exclude: ['pyodide']
